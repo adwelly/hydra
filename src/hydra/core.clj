3c1c38b2e3b7e9bd2bc1f0dc827b8ac7654f8510
Andy Dwelly <andydwelly@gmail.com>
2016-07-03 11:04:25 +0100

    Refactored, replacing paths as encoded strings with vectors

--- src/hydra/core.clj
+++ src/hydra/core.clj
@@ -3,33 +3,26 @@
             [clojure.string :refer [split]]))
 
 (defn to-path-set
-  ([coll] (to-path-set "" coll))
+  ([coll] (to-path-set '[] coll))
   ([path coll]
-   (cond (map? coll)    (reduce union #{} (for [[k v] coll] (to-path-set (str path "/" k) v)))
-         (vector? coll) (reduce union #{} (for [i (range (count coll))] (to-path-set (str path "/" i) (nth coll i))))
-         :else #{(str path "/" coll)})))
+   (cond (map? coll)    (reduce union #{} (for [[k v] coll] (to-path-set (conj path k) v)))
+         (vector? coll) (reduce union #{} (for [i (range (count coll))] (to-path-set (conj path i) (nth coll i))))
+         :else #{(conj path coll)})))
 
-(defn- str->int [s]
-  (Integer. s))
-
-(defn- numeric? [s]
-  (re-matches #"\d+" s))
-
-(defn- every-key-numeric? [m]
-  (every? numeric? (keys m)))
+(defn- every-key-number? [m]
+  (every? number? (keys m)))
 
 (defn- add-paths [mp [hd & tail :as path]]
   (if (= 2 (count path))
-    (assoc mp hd (let [val (second path)] (if (numeric? val) (str->int val) val)))
+    (assoc mp hd (second path))
     (assoc mp hd (add-paths (get mp hd {}) tail))))
 
-(defn- from-path-set-to-map-of-maps [ps]
-  (let [paths (mapv #(-> % (split #"/") rest) ps)]
-    (reduce add-paths {} paths)))
+(defn- from-path-set-to-map-of-maps [path-set]
+  (reduce add-paths {} path-set))
 
 (defn- vectorize [m-of-m]
   (let [sub-maps (into {} (for [[k v] m-of-m] [k (if (map? v) (vectorize v) v)]))]
-    (if (every-key-numeric? sub-maps)
+    (if (every-key-number? sub-maps)
       (map second (sort-by first (into [] sub-maps)))
       sub-maps)))
 
--- test/hydra/core_test.clj
+++ test/hydra/core_test.clj
@@ -29,14 +29,14 @@
   [{"a" 1} {"b" 2} {"c" 3}])
 
 (def numeric-keyed-map
-  {"0" "a"
-   "1" "b"
-   "2" "c"})
+  {0 "a"
+   1 "b"
+   2 "c"})
 
 (def map-with-numeric-keys-representing-vector
-  {"a"   {"0" "b"
-          "1" "c"
-          "2" "d"}
+  {"a"   {0 "b"
+          1 "c"
+          2 "d"}
    "e" 2})
 
 (def deeply-nested-map-with-vectors
@@ -48,31 +48,31 @@
 
 
 (fact "simple map path-set test"
-      (to-path-set simple-map) => #{"/a/1" "/b/2" "/c/3"})
+      (to-path-set simple-map) => #{["a" 1] ["b" 2] ["c" 3]})
 
 (fact "simple vec path-set test"
-      (to-path-set simple-vec) => #{"/0/a" "/1/b" "/2/c"})
+      (to-path-set simple-vec) => #{[0 "a"] [1 "b"] [2 "c"]})
 
 (fact "two level map test"
-      (to-path-set two-level-map) => #{"/a/1" "/b/2" "/c/d/3"})
+      (to-path-set two-level-map) => #{["a" 1] ["b" 2] ["c" "d" 3]})
 
 (fact "A deeply nested map of maps can create a pathmap"
-      (to-path-set deeply-nested-map) => #{"/a/b/1" "/c/2" "/d/e/3" "/d/f/g/4" "/d/f/h/5"})
+      (to-path-set deeply-nested-map) => #{["a" "b" 1] ["c" 2] ["d" "e" 3] ["d" "f" "g" 4] ["d" "f" "h" 5]})
 
 (fact "You can mix maps and vectors"
-      (to-path-set simple-map-with-vector) => #{"/a/0/b" "/a/1/d" "/a/2/e" "/f/2"})
+      (to-path-set simple-map-with-vector) => #{["a" 0 "b"] ["a" 1 "d"] ["a" 2 "e"]  ["f" 2]})
 
 (fact "vectors can be the outermost structure"
-      (to-path-set vector-with-map) => #{"/0/a/1" "/1/b/2" "/2/c/3"})
+      (to-path-set vector-with-map) => #{[0 "a" 1] [1 "b" 2] [2 "c" 3]})
 
 (fact "You can recreate a simple map"
-      (from-path-set-to-map-of-maps #{"/a/1" "/b/2" "/c/3"}) => simple-map)
+      (from-path-set-to-map-of-maps #{["a" 1] ["b" 2] ["c" 3]}) => simple-map)
 
 (fact "You can recreate a two level map"
-      (from-path-set-to-map-of-maps #{"/a/1" "/b/2" "/c/d/3"}) => two-level-map)
+      (from-path-set-to-map-of-maps #{["a" 1] ["b" 2] ["c" "d" 3]}) => two-level-map)
 
 (fact "you can recreate a deeply nested map"
-      (from-path-set-to-map-of-maps #{"/a/b/1" "/c/2" "/d/e/3" "/d/f/g/4" "/d/f/h/5"}) => deeply-nested-map)
+      (from-path-set-to-map-of-maps #{["a" "b" 1] ["c" 2] ["d" "e" 3] ["d" "f" "g" 4] ["d" "f" "h" 5]}) => deeply-nested-map)
 
 (fact "vectorizing maps without numeric keys returns an unchanged map"
       (vectorize simple-map) => simple-map
@@ -86,7 +86,7 @@
       (vectorize map-with-numeric-keys-representing-vector) => {"a" ["b" "c" "d"] "e" 2})
 
 (fact "you can convert a path set into map of maps and vecs"
-      (from-path-set #{"/a/0/b" "/a/1/d" "/a/2/e" "/f/2"}) => simple-map-with-vector)
+      (from-path-set #{["a" 0 "b"] ["a" 1 "d"] ["a" 2 "e"]  ["f" 2]}) => simple-map-with-vector)
 
 (fact "you can make a round trip from maps to path sets and back"
       (-> deeply-nested-map-with-vectors to-path-set from-path-set) => deeply-nested-map-with-vectors)
