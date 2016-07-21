a383378af5273f22fe2736ffc93d38661f23c6bb
adwelly <andydwelly@gmail.com>
2016-07-21 11:35:59 +0100

    Reformatted.

--- test/hydra/core_test.clj
+++ test/hydra/core_test.clj
@@ -74,35 +74,35 @@
       (to-path-map simple-map-with-vector) => simple-map-with-vector-path-map)
 
 (fact "vectors can be the outermost structure"
-             (to-path-map vector-with-map) => {[0 "a"] 1 [1 "b"] 2 [2 "c"] 3})
+      (to-path-map vector-with-map) => {[0 "a"] 1 [1 "b"] 2 [2 "c"] 3})
 
-(fact  "You can recreate a simple map"
+(fact "You can recreate a simple map"
       (from-path-set-to-map-of-maps simple-path-map) => simple-map)
 
-(fact  "You can recreate a two level map"
+(fact "You can recreate a two level map"
       (from-path-set-to-map-of-maps two-level-path-map) => two-level-map)
 
-(fact  "you can recreate a deeply nested map"
+(fact "you can recreate a deeply nested map"
       (from-path-set-to-map-of-maps deeply-nested-path-map) => deeply-nested-map)
 
-(fact  "vectorizing maps without numeric keys returns an unchanged map"
+(fact "vectorizing maps without numeric keys returns an unchanged map"
       (vectorize simple-map) => simple-map
       (vectorize two-level-map) => two-level-map
       (vectorize deeply-nested-map) => deeply-nested-map)
 
-(fact  "vectorizing a map with exclusively numeric keys returns a vector"
+(fact "vectorizing a map with exclusively numeric keys returns a vector"
       (vectorize numeric-keyed-map) => ["a" "b" "c"])
 
-(fact  "vectorizing a map with embeded numeric keys returns a map with an embeded vector"
+(fact "vectorizing a map with embeded numeric keys returns a map with an embeded vector"
       (vectorize map-with-numeric-keys-representing-vector) => {"a" ["b" "c" "d"] "e" 2})
 
 (fact "you can convert a path set into map of maps and vecs"
-             (from-path-map simple-map-with-vector-path-map) => simple-map-with-vector)
+      (from-path-map simple-map-with-vector-path-map) => simple-map-with-vector)
 
 (fact "you can make a round trip from maps to path sets and back"
-             (-> deeply-nested-map-with-vectors to-path-map from-path-map) => deeply-nested-map-with-vectors)
+      (-> deeply-nested-map-with-vectors to-path-map from-path-map) => deeply-nested-map-with-vectors)
 
-(fact  "path creates a path map representing a simple path"
+(fact "path creates a path map representing a simple path"
       (path [1 2 3]) => {[1 2] 3}
       (path '("a" "b" "c")) => {["a" "b"] "c"})
 
@@ -110,13 +110,13 @@
   (> (count path) len))
 
 (fact "You can cleave a set of paths into two with a predicate"
-             (-> (cleave (partial path-longer-than? 3) (to-path-map deeply-nested-map-with-vectors)) first) =>
-             {["d" "f" "h" 2] 7 ["d" "f" "h" 1] 6 ["d" "f" "g"] 4 ["d" "f" "h" 0] 5}
-             (-> (cleave (partial path-longer-than? 3) (to-path-map deeply-nested-map-with-vectors)) second) =>
-             {["a" "b"] 1 ["c" 2] "k" ["c" 0] "i" ["d" "e"] 3 ["c" 1] "j"})
+      (-> (cleave (partial path-longer-than? 3) (to-path-map deeply-nested-map-with-vectors)) first) =>
+      {["d" "f" "h" 2] 7 ["d" "f" "h" 1] 6 ["d" "f" "g"] 4 ["d" "f" "h" 0] 5}
+      (-> (cleave (partial path-longer-than? 3) (to-path-map deeply-nested-map-with-vectors)) second) =>
+      {["a" "b"] 1 ["c" 2] "k" ["c" 0] "i" ["d" "e"] 3 ["c" 1] "j"})
 
 (fact "Splicing is the inverse of cleaving"
-             (->> deeply-nested-map-with-vectors to-path-map (cleave (partial path-longer-than? 3)) (apply merge) from-path-map) => deeply-nested-map-with-vectors)
+      (->> deeply-nested-map-with-vectors to-path-map (cleave (partial path-longer-than? 3)) (apply merge) from-path-map) => deeply-nested-map-with-vectors)
 
 (fact "splicing can update values, (c moves from 3 to 4)"
       (-> (merge simple-path-map {["c"] 4}) from-path-map) => {"a" 1 "b" 2 "c" 4})
@@ -124,44 +124,44 @@
 (fact "prepend path takes a pair representing a path and value, from one map and a pair representing the path and value from a second map and prepends the first to the second"
       (prepend-path [[:a :b] :c] [[:d :e] :f]) => [[:a :b :c :d :e] :f])
 
-(fact  "Cross product applies a binary function taking two pairs and returning a pair, to every combinations of path/value in two path maps"
-      (cross-product prepend-path  {[:a] 1 [:b] 2} {[:c] 3 [:d] 4}) => {[:a 1 :c] 3 [:a 1 :d] 4 [:b 2 :c] 3 [:b 2 :d] 4})
+(fact "Cross product applies a binary function taking two pairs and returning a pair, to every combinations of path/value in two path maps"
+      (cross-product prepend-path {[:a] 1 [:b] 2} {[:c] 3 [:d] 4}) => {[:a 1 :c] 3 [:a 1 :d] 4 [:b 2 :c] 3 [:b 2 :d] 4})
 
 (future-fact "modifying leaf"
              (-> deeply-nested-map-with-vectors to-path-map (reset-leaf ["a"] 42) from-path-map) =>
              {"a" {"b" 42}
-       "c" ["i" "j" "k"]
-       "d" {"e" 3
-            "f" {"g" 4
-                 "h" [5 6 7]}}})
+              "c" ["i" "j" "k"]
+              "d" {"e" 3
+                   "f" {"g" 4
+                        "h" [5 6 7]}}})
 
 (future-fact "transforming leaves"
              (-> deeply-nested-map-with-vectors to-path-map (transform-leaf ["d"] inc) from-path-map) =>
              {"a" {"b" 1}
-       "c" ["i" "j" "k"]
-       "d" {"e" 4
-            "f" {"g" 5
-                 "h" [6 7 8]}}})
+              "c" ["i" "j" "k"]
+              "d" {"e" 4
+                   "f" {"g" 5
+                        "h" [6 7 8]}}})
 
-(future-fact  "The starts-with? function identifies paths starting with a given route"
-      (starts-with? [] ["a" 0 "b"]) => true
-      (starts-with? ["a" 0] ["a" 0 "b"]) => true
-      (starts-with? ["a" number?] ["a" 0 "b"]) => true
-      (starts-with? ["a" 1] ["a" 0 "b"]) => false
-      (starts-with? ["a" string?] ["a" 0 "b"]) => false)
+(future-fact "The starts-with? function identifies paths starting with a given route"
+             (starts-with? [] ["a" 0 "b"]) => true
+             (starts-with? ["a" 0] ["a" 0 "b"]) => true
+             (starts-with? ["a" number?] ["a" 0 "b"]) => true
+             (starts-with? ["a" 1] ["a" 0 "b"]) => false
+             (starts-with? ["a" string?] ["a" 0 "b"]) => false)
 
-(future-fact  "The ends-with? function identifies paths starting with a given route"
-      (ends-with? [] ["a" 0 "b"]) => true
-      (ends-with? [0 "b"] ["a" 0 "b"]) => true
-      (ends-with? [number? "b"] ["a" 0 "b"]) => true
-      (ends-with? [1 "b"] ["a" 0 "b"]) => false
-      (ends-with? [string? "b"] ["a" 0 "b"]) => false)
+(future-fact "The ends-with? function identifies paths starting with a given route"
+             (ends-with? [] ["a" 0 "b"]) => true
+             (ends-with? [0 "b"] ["a" 0 "b"]) => true
+             (ends-with? [number? "b"] ["a" 0 "b"]) => true
+             (ends-with? [1 "b"] ["a" 0 "b"]) => false
+             (ends-with? [string? "b"] ["a" 0 "b"]) => false)
 
 (fact "upsert allows structures to be inserted into other structures"
       (let [deep (to-path-map deeply-nested-map-with-vectors)
             simple (to-path-map simple-map)]
-               (-> (upsert (path ["d" "f" "i"]) deep simple) from-path-map)) =>
-             {"a" {"b" 1}
+        (-> (upsert (path ["d" "f" "i"]) deep simple) from-path-map)) =>
+      {"a" {"b" 1}
        "c" ["i" "j" "k"]
        "d" {"e" 3
             "f" {"g" 4
@@ -172,24 +172,24 @@
 
 (future-fact "upsert allows vectors to be created on the fly"
              (let [deep (to-path-map deeply-nested-map-with-vectors)
-            simple (to-path-map simple-map)]
+                   simple (to-path-map simple-map)]
                (-> (upsert (path ["d" "f" "i" 0]) deep simple) from-path-map)) =>
              {"a" {"b" 1}
-         "c" ["i" "j" "k"]
-         "d" {"e" 3
-              "f" {"g" 4
-                   "h" [5 6 7]
-                   "i" [{"a" 1
-                         "b" 2
-                         "c" 3}]}}})
+              "c" ["i" "j" "k"]
+              "d" {"e" 3
+                   "f" {"g" 4
+                        "h" [5 6 7]
+                        "i" [{"a" 1
+                              "b" 2
+                              "c" 3}]}}})
 
 
-(future-fact  "append-next appends elements to paths"
-      (append-next [] "a") => [["a"]]
-      (append-next [["a"] ["a" "b"]] "c") => [["a"] ["a" "b"] ["a" "b" "c"]])
+(future-fact "append-next appends elements to paths"
+             (append-next [] "a") => [["a"]]
+             (append-next [["a"] ["a" "b"]] "c") => [["a"] ["a" "b"] ["a" "b" "c"]])
 
-(future-fact  "all-sub-paths-of-path produces all subpaths"
-      (all-subpaths-of-path []) => []
-      (all-subpaths-of-path ["a"]) => [["a"]]
-      (all-subpaths-of-path ["a" "b" "c"]) => [["a"] ["a" "b"] ["a" "b" "c"]])
+(future-fact "all-sub-paths-of-path produces all subpaths"
+             (all-subpaths-of-path []) => []
+             (all-subpaths-of-path ["a"]) => [["a"]]
+             (all-subpaths-of-path ["a" "b" "c"]) => [["a"] ["a" "b"] ["a" "b" "c"]])
 
